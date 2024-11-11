a6efd66cd85084b9d5fa5fc881aa9cad16541aa0
mukarrom21 <mukarrom21@gmail.com>
2024-11-11 23:09:17 +0600

    profile update completed

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -5,6 +5,7 @@ import 'package:tm_getx/ui/controller/reset_password_controller.dart';
 import 'package:tm_getx/ui/controller/sign_up_controller.dart';
 import 'package:tm_getx/ui/controller/task_list_controller.dart';
 import 'package:tm_getx/ui/controller/task_summary_controller.dart';
+import 'package:tm_getx/ui/controller/update_profile_controller.dart';
 import 'package:tm_getx/ui/controller/verify_email_controller.dart';
 import 'package:tm_getx/ui/controller/verify_otp_controller.dart';
 
@@ -17,6 +18,7 @@ class ControllerBinder extends Bindings {
     Get.put(VerifyEmailController());
     Get.put(VerifyOtpController());
     Get.put(ResetPasswordController());
+    Get.put(UpdateProfileController());
     Get.put(TaskListController());
     Get.put(TaskSummaryController());
   }
